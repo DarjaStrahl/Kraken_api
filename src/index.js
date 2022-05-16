ad5074e83887163dc5aaa6b746a6bb675b581b5a
Darja Strahlberg <darja.strahlberg@gmail.com>
2022-05-16 16:03:44 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,63 @@
+const key          = process.env.KRAKEN_API_KEY; // API Key
+const secret       = process.env.KRAKEN_SECRET_KEY; // API Private Key
+const KrakenClient = require('kraken-api');
+const kraken       = new KrakenClient(key, secret);
+
+async function wait() {
+    return new Promise((resolve) => setTimeout(resolve, 2000))
+}
+
+//const INVEST_AMOUNT = 100
+
+const configs = [{
+    ticker: "XXBTZEUR",
+    amount: 100,
+    },
+]
+
+;(async () => {
+    // Display user's balance
+    let eurBalance = (await kraken.api('Balance')).result.ZEUR
+    console.log( 'eurBalance', eurBalance );
+
+    // Get Ticker Info
+    const tickerInfo = await kraken.api('Ticker', { pair : "XXBTZEUR" })
+    //const tickerInfo = await kraken.api('Ticker', { pair : 'XXBTZEUR' });
+
+    configs.forEach(pair => {
+        pair.currentPrice =parseFloat(tickerInfo.result[pair.ticker].a[0])
+    })
+
+
+    for(const pair of configs){
+        await wait()
+
+        const volume = (pair.amount/ pair.currentPrice).toFixed(6)
+        const price = (pair.currentPrice * 0.5).toFixed(6)
+        console.log( "price: ", price )
+        console.log( "volume: ", volume )
+        console.log("Trying to place an order for " + pair.ticker)
+        if ( eurBalance - pair.amount < 100){
+            console.log("Insufficient funds, abort.")
+            return
+        }
+        console.log( 'volume', volume )
+        console.log( "price", price )
+        try {
+            const result = await kraken.api('AddOrder', {
+                pair: pair.ticker,
+                type: 'buy',
+                ordertype: 'limit',
+                price,
+                volume,
+                expiretm: '+${60 * 60 * 24)',
+            })
+            eurBalance -= pair.amount
+            console.log( "result", result )
+        } catch (err) {
+            console.error(err)
+        }
+
+    }
+
+})();
